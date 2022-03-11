6a3ab9a3bf6e376941c40278c759badd8d13412d
ckvbinfrrd <chaitanyakrishna@infrrd.ai>
2022-03-12 00:16:44 +0530

    removed timeout

--- App/services/split_pdf_service.py
+++ App/services/split_pdf_service.py
@@ -14,7 +14,7 @@ def split_pdf_service(file):
         os.path.join(cs.IMG_DIR, file_name)
     ]
     try:
-        subprocess.check_output(command, timeout=1000)
+        subprocess.check_output(command)
     except Exception as e:
         return JSONResponse(
                 content={
